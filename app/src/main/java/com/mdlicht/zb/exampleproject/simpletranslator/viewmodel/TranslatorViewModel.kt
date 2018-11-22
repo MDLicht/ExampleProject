02b9359369fda6d08b8ada2fca40d13473ff6c2d
Kim <zombie612@naver.com>
2018-11-22 23:55:57 +0900

    Add copy to clipboard on Simple Translator

--- app/src/main/java/com/mdlicht/zb/exampleproject/simpletranslator/viewmodel/TranslatorViewModel.kt
+++ app/src/main/java/com/mdlicht/zb/exampleproject/simpletranslator/viewmodel/TranslatorViewModel.kt
@@ -3,10 +3,17 @@ package com.mdlicht.zb.exampleproject.simpletranslator.viewmodel
 import android.arch.lifecycle.Lifecycle
 import android.arch.lifecycle.OnLifecycleEvent
 import android.arch.lifecycle.ViewModel
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.databinding.ObservableField
-import android.util.Log
+import android.text.TextUtils
+import android.view.View
+import android.widget.Toast
+import com.mdlicht.zb.exampleproject.R
 import com.mdlicht.zb.exampleproject.simpletranslator.api.RetrofitUtil
 import com.mdlicht.zb.exampleproject.simpletranslator.model.Require
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
@@ -48,5 +55,40 @@ class TranslatorViewModel : ViewModel() {
 
     fun onTextChanged(text: CharSequence) {
         publishSubject.onNext(text.toString())
+    }
+
+    fun onResultLongClick(view: View): Boolean {
+        destText.get()?.let {
+            compositeDisposable.add(
+                Observable.just(it)
+                    .filter { text ->
+                        !TextUtils.isEmpty(text)
+                    }
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe { text ->
+                        run {
+                            val clipBoard: ClipboardManager =
+                                view.context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                            val clipData: ClipData = ClipData.newPlainText(
+                                view.context.getString(R.string.simple_translator_copy_clipboard),
+                                text
+                            )
+                            clipBoard.primaryClip = clipData
+                            Toast.makeText(
+                                view.context,
+                                view.context.getString(R.string.simple_translator_msg_copy_clipboard),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+            )
+        } ?: run {
+            Toast.makeText(
+                view.context,
+                view.context.getString(R.string.simple_translator_msg_empty_text),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        return true
     }
 }
