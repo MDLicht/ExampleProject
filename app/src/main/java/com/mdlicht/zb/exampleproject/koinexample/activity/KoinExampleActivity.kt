8fe2c52f4b531aa2fe47072d1d0263865e0cc04d
Kim <zombie612@naver.com>
2019-05-14 23:51:14 +0900

    Databinding by koin

--- app/src/main/java/com/mdlicht/zb/exampleproject/koinexample/activity/KoinExampleActivity.kt
+++ app/src/main/java/com/mdlicht/zb/exampleproject/koinexample/activity/KoinExampleActivity.kt
@@ -1,6 +1,5 @@
 package com.mdlicht.zb.exampleproject.koinexample.activity
 
-import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
@@ -8,15 +7,16 @@ import com.mdlicht.zb.exampleproject.R
 import com.mdlicht.zb.exampleproject.databinding.ActivityKoinExampleBinding
 import com.mdlicht.zb.exampleproject.koinexample.viewmodel.KoinViewModel
 import com.mdlicht.zb.exampleproject.rxbasic.adapter.RxBasicRecyclerAdapter
+import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.viewModel
+import org.koin.core.parameter.parametersOf
 
 class KoinExampleActivity : AppCompatActivity() {
-    lateinit var binding: ActivityKoinExampleBinding
+    val binding: ActivityKoinExampleBinding by inject { parametersOf(this, R.layout.activity_koin_example) }
     val vm: KoinViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_koin_example)
         binding.apply {
             vm = this@KoinExampleActivity.vm
 
--- app/src/main/java/com/mdlicht/zb/exampleproject/koinexample/di/KoinModule.kt
+++ app/src/main/java/com/mdlicht/zb/exampleproject/koinexample/di/KoinModule.kt
@@ -1,6 +1,9 @@
 package com.mdlicht.zb.exampleproject.koinexample.di
 
+import android.databinding.DataBindingUtil
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import com.mdlicht.zb.exampleproject.databinding.ActivityKoinExampleBinding
+import com.mdlicht.zb.exampleproject.koinexample.activity.KoinExampleActivity
 import com.mdlicht.zb.exampleproject.koinexample.repository.KoinRepository
 import com.mdlicht.zb.exampleproject.koinexample.viewmodel.KoinViewModel
 import com.mdlicht.zb.exampleproject.rxbasic.api.RxBasicService
@@ -22,6 +25,10 @@ val appModule = module {
 
     single {
         KoinRepository(get())
+    }
+
+    factory {
+        (activity: KoinExampleActivity, layout: Int) -> DataBindingUtil.setContentView(activity, layout) as ActivityKoinExampleBinding
     }
 
     viewModel {
